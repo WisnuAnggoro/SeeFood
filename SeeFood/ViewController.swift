c424288e320898b212e609c735703d26cb241efe
Wisnu Anggoro <wisnu@anggoro.net>
2018-06-20 15:52:45 +0700

    Detect image that is taken by camera whether it's hotdog or not

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -28,9 +28,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!!!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog :("
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
         
     }
 
